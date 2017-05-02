519232bee02cc405f9cfdd0de3f80538395051fd
Péter Kőrössy <meanwhile@vipmail.hu>
2017-05-02 19:00:57 +0200

    created the needed files

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template, redirect, url_for
+import common
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
