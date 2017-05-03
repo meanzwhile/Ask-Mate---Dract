fd23832bcd9e3286f9d847fab656e4bbd57ce9de
Péter Kőrössy <meanwhile@vipmail.hu>
2017-05-03 13:55:25 +0200

    HTML files ready

--- main.py
+++ main.py
@@ -11,5 +11,18 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/answer/<id>", methods=["GET", "POST"])
+def answer(id):
+    return render_template("answer.html",
+                           question_title="Which color is my fav?",
+                           message="Blue",
+                           answer_list=["Its my good answer! asdasf sdfsdnsdfk  dksdf  klsd  jklsd jklsd éljkas  éksd géklsgéklsd géks éksdgéksgdklsd fgékfsdfgdf gsdj iljsdf kljsdfkljsdklfjsdf kl", "500", "2014.04.03"])
+
+
+@app.route("/update/<id>", methods=["GET", "POST"])
+def update(id):
+    return render_template("update.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
