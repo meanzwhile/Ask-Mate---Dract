77160b9ab2f48c5653ed54441a2d176ddd7f6b7c
maylandertamas <maylandertamas@gmail.com>
2017-05-03 14:34:15 +0200

    Added encode and decode function

--- common.py
+++ common.py
@@ -1,17 +1,21 @@
-from base64 import encode, decode
+from base64 import b64decode, b64encode
 
 
-def get_table_from_file(file_name="stories.csv"):
+def get_table_from_file(file_name):
     with open(file_name, "r") as file:
         lines = file.readlines()
-    table = [element.replace("\n", "").split(";") for element in lines]
-    return table
+    table = [element.replace("\n", "").split(",") for element in lines]
+    decoded_table = encode_decode_b64(table, "decode")
+    print(decoded_table)
+    return decoded_table
 
 
-def write_table_to_file(table, file_name="stories.csv"):
+def write_table_to_file(table, file_name):
+    encoded_table = encode_decode_b64(table, "encode")
+    print(encoded_table)
     with open(file_name, "w") as file:
         for record in table:
-            row = ';'.join(record)
+            row = ','.join(record)
             file.write(row + "\n")
 
 
@@ -21,3 +25,25 @@ def ID_generator(table):
         return str(1)
     else:
         return str(max(IDs_in_table) + 1)
+
+
+def encode_decode_b64(table, method):
+    if method == "decode":
+        for element in table:
+            for idx, item in enumerate(element):
+                if idx == 4:
+                    element[4] = b64decode(element[4]).decode("utf-8")
+                if idx == 5:
+                    element[5] = b64decode(element[5]).decode("utf-8")
+                if idx == 6:
+                    element[6] = b64decode(element[6]).decode("utf-8")
+    if method == "encode":
+        for element in table:
+            for idx, item in enumerate(element):
+                if idx == 4:
+                    element[4] = b64encode(bytes(element[4], 'utf-8')).decode("utf-8")
+                if idx == 5:
+                    element[5] = b64encode(bytes(element[5], "utf-8")).decode("utf-8")
+                if idx == 6:
+                    element[6] = b64encode(bytes(element[6], "utf-8")).decode("utf-8")
+    return table
