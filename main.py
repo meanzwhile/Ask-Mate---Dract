06fc6990dcbf14cc1f819d47315a9826cb1d171e
maylandertamas <maylandertamas@gmail.com>
2017-05-03 09:46:00 +0200

    Working now with file

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    data_table = common.get_table_from_file("question.csv")
-    return render_template("index.html")
+    data_table = common.get_table_from_file("data/question.csv")
+    return render_template("index.html", data_list=data_table)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
