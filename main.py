8d21158692e554bd816c5d22bd8e2e0bc33ca683
maylandertamas <maylandertamas@gmail.com>
2017-05-04 15:51:52 +0200

    Update question 's working now

--- main.py
+++ main.py
@@ -99,10 +99,22 @@ def update_page(question_id):
 
 @app.route("/submit-update/<question_id>", methods=["POST"])
 def update_data(question_id):
-    # unfinished! should add new list to file
     question_table = common.get_table_from_file("data/question.csv")
-    new_data = []
-    new_data.append(request.form[''])
+    updated_question = []
+    for element in question_table:
+        if element[0] == question_id:
+            updated_question.append(element)
+            question_table.remove(element)
+    flatten_updated_question = []
+    for element in updated_question:
+        for item in element:
+            flatten_updated_question.append(item)
+    flatten_updated_question.pop(4)
+    flatten_updated_question.pop(4)
+    flatten_updated_question.insert(4, request.form['updated-question_title'])
+    flatten_updated_question.insert(5, request.form['updated-message'])
+    question_table.append(flatten_updated_question)
+    common.write_table_to_file(question_table, "data/question.csv")
     return redirect(url_for('answer', question_id=question_id))
 
 
